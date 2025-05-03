a6fee5ee43f7b608054060793afa72ecd1eaf777
Ben Walker <b6nw4r@gmail.com>
2025-05-03 16:35:30 -0400

    configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import noBarrelFiles from 'eslint-plugin-no-barrel-files';
+import reactDom from 'eslint-plugin-react-dom';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  stylistic.configs.customize({
+    indent: 2,
+    jsx: true,
+    quotes: 'single',
+    semi: true,
+  }),
+  noBarrelFiles.flat,
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -23,6 +44,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'sort-imports': ['error'],
     },
   },
-)
+);
